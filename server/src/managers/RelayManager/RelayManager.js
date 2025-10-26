aea2c7a85cbb667f3333c3bb673cccad16585be4
Yevgeniy Verchenko <verchenkoyevgeniy@gmail.com>
2025-10-26 21:41:07 +0300

    orange

--- server/src/managers/RelayManager/RelayManager.js
+++ server/src/managers/RelayManager/RelayManager.js
@@ -9,10 +9,10 @@ export class RelayManager {
     onRelaysStateChanged = null;
 
     constructor(onRelaysStateChanged) {
-        this.pumpRelay = new Relay('Насос', 33, false);
-        this.lightRelay = new Relay('Свет', 25, false);
-        this.airRelay = new Relay('Аэратор', 26, false);
-        this.fanRelay = new Relay('Вентилятор', 27, false);
+        this.pumpRelay = new Relay('Насос', 354, false);
+        this.lightRelay = new Relay('Свет', 360, false);
+        this.airRelay = new Relay('Аэратор', 362, false);
+        this.fanRelay = new Relay('Вентилятор', 118, false);
         this.onRelaysStateChanged = onRelaysStateChanged;
     }
 
